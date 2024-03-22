f03f6bf7c1e9c61d5ba8470963dd4cad4fb4ea08
Zakhar Stakh <stakh.zakhar@chnu.edu.ua>
2024-03-22 04:59:33 +0200

    Task 6 done.

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,3 +1,6 @@
 def int_within_bounds(number, lower_bound, upper_bound):
   # Type your code
-  return 
+  if number >= lower_bound and number < upper_bound and type(number) == int:
+   return True
+  elif number < lower_bound or number >= upper_bound or type(number) == float:
+    return False
