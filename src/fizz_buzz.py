f48059a23ac6471dc216cd2e0563eacd5becfb85
Zakhar Stakh <stakh.zakhar@chnu.edu.ua>
2024-03-22 04:10:06 +0200

    Task 0 done.

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,11 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+  if param % 3 == 0 and % 5 == 0
+      result = "Fizz"
+  elif param % 5 == 0
+      result = "Buzz"
+  elif param % 3 == 0
+      result = "FizzBuzz"
+  else:
+result = str(param)
+  return (result)
