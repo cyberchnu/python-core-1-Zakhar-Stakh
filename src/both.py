bee0e949a9aa1ee0f1a659b4cd2bfe56b7b42e95
Zakhar Stakh <stakh.zakhar@chnu.edu.ua>
2024-03-22 04:46:35 +0200

    Task 5 done.

--- src/both.py
+++ src/both.py
@@ -1,3 +1,7 @@
 def both(number1, number2):
   # Type your code
+  if both(number1 > 0, number2 > 0) or both(number1 < 0, number2 < 0) or both(number1 == 0, number2 == 0):
+    return True
+  else:
+    return False
   return
