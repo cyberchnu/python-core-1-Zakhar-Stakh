287f33630f4999b50ef6608438832decfe58aea0
Zakhar Stakh <stakh.zakhar@chnu.edu.ua>
2024-05-27 16:17:48 +0300

    10

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,5 +1,5 @@
 def calculate_fuel(distance):
     if distance <= 0:
         raise ValueError("Distance must be greater than 0")
-    fuel = distance / 10
+    fuel = distance / 10.0  # Додавання десяткового роздільника, щоб результат був у форматі з плаваючою точкою
     return fuel
